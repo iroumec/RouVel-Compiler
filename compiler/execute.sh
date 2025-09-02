ca7c363301ad73a99fdec723c410eb171a3a1c03
iroumec <inaki.roumec@gmail.com>
2025-09-01 21:34:31 -0300

    Improvements

--- compiler/execute.sh
+++ compiler/execute.sh
@@ -4,7 +4,7 @@ DIR="$(dirname "$0")/src"
 cd "$DIR" || exit 1
 
 # Se compila el programa y se ejecuta.
-javac app/Main.java && java Main
+javac Main.java && java Main
 
 # Terminada la ejecución, se buscan y eliminar todos los.class dentro del subdirectorio.
 find "$DIR" -type f -name "*.class" -delete
--- compiler/src/Main.java
+++ compiler/src/Main.java
@@ -1,4 +1,7 @@
-package app;
+
+import app.DataLoader;
+import app.LexicalAnalyzer;
+import app.Token;
 
 public class Main {
     public static void main(String[] args) {
